5419f26d891f810d5cf3ccd8578d53479932507d
Pushkar Singh Rawat <pushkars@amdocs.com>
2019-10-24 21:05:07 +0530

    updated customer model.

--- src/main/java/springkafka/kafkademo/model/Customer.java
+++ src/main/java/springkafka/kafkademo/model/Customer.java
@@ -1,8 +1,15 @@
 package springkafka.kafkademo.model;
 
-public class Customer {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.stereotype.Component;
 
+@Component
+@Document(indexName = "localelastic",type="customer")
+public class Customer {
+   
 	private String customerName;
+	@Id
     private String id;
     
 	public String getCustomerName() {
