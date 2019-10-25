75259e7acc71988387f529046b0d8af67a4c14bc
Pushkar Singh Rawat <pushkars@amdocs.com>
2019-10-25 20:10:37 +0530

    updated customer repo

--- src/main/java/springkafka/kafkademo/persistence/CustomerRepository.java
+++ src/main/java/springkafka/kafkademo/persistence/CustomerRepository.java
@@ -1,9 +1,12 @@
 package springkafka.kafkademo.persistence;
 
+import java.util.List;
+
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import springkafka.kafkademo.model.Customer;
 
 public interface CustomerRepository extends ElasticsearchRepository<Customer, String>{
 
+	public List<Customer> findByCustomerName(String name);
 }
